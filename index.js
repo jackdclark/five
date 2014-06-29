fa7da2ed5a5097c6532b39860b77222a9f22b139
vikramsoni2 <vikram9880@gmail.com>
2014-06-30 00:37:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = five;
 five.upHigh = function() { return '⁵'; };
 five.downLow = function() { return '₅'; };
 five.roman = function() { return 'V'; };
+five.english = function() { return 'Five' };
 
 five.tooSlow = function() {
   var returnIn = new Date(new Date().valueOf() + 555);
