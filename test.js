606cb5a8595d32c853294b877e6d4498996b2fee
Giorgio Sintichakis <gsintichakis@gmail.com>
2014-07-17 19:02:37 -0400

    Update test.js
    Wasn't paying attention...

--- test.js
+++ test.js
@@ -29,9 +29,9 @@ assert.equal('di-di-di-di-dit', five.morseCode(), 'A five in morse code should b
 assert.equal('101', five.binary(), 'A binary five should be 101');
 assert.equal('5', five.octal(), 'An octal five should be 5');
 
-assert.equal('negative', five.negative(), 'A negative five should be -5');
-assert.equal('loud', five.loud(), 'A loud five should be FIVE');
-assert.equal('smooth', five.smooth(), 'A smooth five should be S');
+assert.equal('-5', five.negative(), 'A negative five should be -5');
+assert.equal('FIVE', five.loud(), 'A loud five should be FIVE');
+assert.equal('S', five.smooth(), 'A smooth five should be S');
 
 var now = new Date().valueOf();
 var slowFive = five.tooSlow();
