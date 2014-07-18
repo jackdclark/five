7fbc521a93a3ef1d9d3be6ddae5040e71e3f9132
jFox <jfoxsingularity@gmail.com>
2014-07-18 14:00:57 +0400

    Update test.js

--- test.js
+++ test.js
@@ -18,6 +18,7 @@ assert.equal('fünf', five.german(), 'A german five should be fünf');
 assert.equal('cúig', five.irish(), 'A irish five should be cúig');
 assert.equal('таван', five.mongolian(), 'A mongolian five should be таван');
 assert.equal('pięć', five.polish(), 'A polish five should be pięć');
+assert.equal('pięć', five.russian(), 'A russian five should be пять');
 assert.equal('pet', five.slovenian(), 'A slovenian five should be pet');
 assert.equal('fem', five.swedish(), 'A swedish five should be fem');
 assert.equal('ห้า', five.thai(), 'A thai five should be ห้า');
