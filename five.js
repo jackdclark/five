ee917419ab4179234989552dd185233bdf85cae7
Marcel Eeken <marcel.eeken@gmail.com>
2014-07-18 11:02:17 +0200

    Update five.js
    Added dutch translation

--- five.js
+++ five.js
@@ -23,6 +23,7 @@
   five.piglatin = function() { return 'ivefay' };
   five.italian = function() { return 'cinque' };
   five.spanish = function() { return 'cinco' };
+  five.dutch = function() { return 'vijf' };
 
 
   five.morseCode = function() { return 'di-di-di-di-dit' };
