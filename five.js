a703bdc082777659458af934d076eb7cf601a16e
Xnuk Tatsukei <limm1127@gmail.com>
2014-07-18 12:51:50 +0900

    Korean added

--- five.js
+++ five.js
@@ -8,21 +8,22 @@
 
   five.arabic = function() { return 'خمسة' };
   five.croatian = function() { return 'pet' };
+  five.choctaw = function() { return 'tahlapi' };
   five.czech = function() { return 'pět' };
   five.english = function() { return 'Five' };
   five.finnish = function() { return 'viisi' };
   five.french = function() { return 'cinq' };
   five.german = function() { return 'fünf' };
   five.irish = function() { return 'cúig' };
+  five.korean = function() { return '오' };
   five.mongolian = function() { return 'таван' };
   five.polish = function() { return 'pięć' };
   five.slovenian = function() { return 'pet' };
+  five.spanish = function() { return 'cinco' };
   five.swedish = function() { return 'fem' };
   five.thai = function() { return 'ห้า' };
-  five.choctaw = function() { return 'tahlapi' };
   five.piglatin = function() { return 'ivefay' };
   five.italian = function() { return 'cinque' };
-  five.spanish = function() { return 'cinco' };
 
 
   five.morseCode = function() { return 'di-di-di-di-dit' };
