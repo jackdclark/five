e5856b8380fe023153ed1875e8990c309c93b4c3
jackdcrawford <jackcrawford19@gmail.com>
2014-07-22 20:17:46 +0100

    Semicolons removed in a previous merge, adding back in

--- five.js
+++ five.js
@@ -6,34 +6,34 @@
   five.downLow = function() { return '₅'; };
   five.roman = function() { return 'V'; };
 
-  five.arabic = function() { return 'خمسة' };
-  five.croatian = function() { return 'pet' };
-  five.czech = function() { return 'pět' };
-  five.dutch = function() { return 'vijf' };
-  five.english = function() { return 'Five' };
-  five.finnish = function() { return 'viisi' };
-  five.french = function() { return 'cinq' };
-  five.german = function() { return 'fünf' };
-  five.irish = function() { return 'cúig' };
-  five.mongolian = function() { return 'таван' };
-  five.polish = function() { return 'pięć' };
-  five.slovenian = function() { return 'pet' };
-  five.swedish = function() { return 'fem' };
-  five.thai = function() { return 'ห้า' };
-  five.choctaw = function() { return 'tahlapi' };
-  five.piglatin = function() { return 'ivefay' };
-  five.italian = function() { return 'cinque' };
-  five.spanish = function() { return 'cinco' };
+  five.arabic = function() { return 'خمسة'; };
+  five.croatian = function() { return 'pet'; };
+  five.czech = function() { return 'pět'; };
+  five.dutch = function() { return 'vijf'; };
+  five.english = function() { return 'Five'; };
+  five.finnish = function() { return 'viisi'; };
+  five.french = function() { return 'cinq'; };
+  five.german = function() { return 'fünf'; };
+  five.irish = function() { return 'cúig'; };
+  five.mongolian = function() { return 'таван'; };
+  five.polish = function() { return 'pięć'; };
+  five.slovenian = function() { return 'pet'; };
+  five.swedish = function() { return 'fem'; };
+  five.thai = function() { return 'ห้า'; };
+  five.choctaw = function() { return 'tahlapi'; };
+  five.piglatin = function() { return 'ivefay'; };
+  five.italian = function() { return 'cinque'; };
+  five.spanish = function() { return 'cinco'; };
 
 
-  five.morseCode = function() { return 'di-di-di-di-dit' };
+  five.morseCode = function() { return 'di-di-di-di-dit'; };
   five.binary = function() { return '101'; };
   five.octal = function() { return '5'; };
-  
+
   five.negative = function() { return -5; }
   five.loud = function() { return "FIVE"; }
   five.smooth = function() { return "S"; }
-  
+
   five.tooSlow = function() {
     var returnIn = new Date(new Date().valueOf() + 555);
 
