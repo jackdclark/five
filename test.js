2fb6edc3b7a3ae41e18eb689e7f14960c963e6d7
atomic1fire <atomic1fire@gmail.com>
2015-01-13 05:57:36 -0600

    update test.js with Jackson 5.

--- test.js
+++ test.js
@@ -75,6 +75,9 @@ var finishes = new Date().valueOf();
 assert.equal(5, slowFive, 'A too slow five should still be five');
 assert.ok((finishes - now) > 500, 'A too slow five should take longer than 500 milliseconds to be returned, blocking execution and generally being a bad idea');
 
+
+assert.equal(JSON.stringify(['Jackie','Tito','Jermaine','Marlon','Michael']), JSON.stringify(five.fab()), 'A Jackson five should be the five members of the 70\'s pop family group the Jackson 5');
+
 assert.equal(JSON.stringify(['Juwan Howard','Ray Jackson','Jimmy King','Jalen Rose','Chris Webber']), JSON.stringify(five.fab()), 'A fab five should be the 1991-1993 Michigan Mens Basketball Team');
 
 assert.equal(JSON.stringify([5, 5, 5]), JSON.stringify(five.map([1, 2, 3])));
