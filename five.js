52bcd8dde7787977915c071eb3c0f0e0bb2906c3
Mustafa Hanif <mhanif@folio3.com>
2015-05-04 16:03:40 +0500

    Added Urdu five

--- five.js
+++ five.js
@@ -65,6 +65,7 @@
   five.turkish = function() { return 'beş'; };
   five.thai = function() { return 'ห้า'; };
   five.ukrainian = function() { return 'п’ять'; };
+  five.urdu = function() { return 'پانچ'; };
   five.welsh = function() { return 'pump'; };
   
   five.morseCode = function() { return 'di-di-di-di-dit'; };
