44daf440eae0cc6ba18a14a2dbdc3e9b6d41d49b
Martin Trobäck <martintk@axis.com>
2015-05-05 12:33:15 +0200

    You can now be loud in all languages

--- five.js
+++ five.js
@@ -73,7 +73,6 @@
   five.hex = function() { return '5'; };
 
   five.negative = function() { return -5; };
-  five.loud = function() { return 'FIVE'; };
   five.loud = function (lang) { return (lang && typeof five[lang] === 'function') ? five[lang]().toUpperCase() : five.english().toUpperCase();};
   five.smooth = function() { return 'S'; };
 
