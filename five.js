be09d0b25138c5fc473d1230938199ce7d36dcfd
Yuanfei Zhu <yuanfeiz@everstring.com>
2016-03-25 16:45:43 -0700

    Add traditional chinese

--- five.js
+++ five.js
@@ -68,6 +68,7 @@
   five.swedish = function() { return 'fem'; };
   five.tamil = function() { return 'ஐந்து'; };
   five.telugu = function() { return 'ఐదు'; };
+  five.traditionalChinese = function() { return '伍'; };
   five.turkish = function() { return 'beş'; };
   five.thai = function() { return 'ห้า'; };
   five.ukrainian = function() { return 'п’ять'; };
