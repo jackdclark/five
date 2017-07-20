de19c48148022f18580258a686101ce69b51076d
Naveen Pai <naveen.pai@bloomreach.com>
2017-07-20 19:12:22 +0000

    Adding the five elements as per Japanese buddhist philosophy

--- five.js
+++ five.js
@@ -150,6 +150,10 @@
     return ['Slam Dunk (Da Funk)', 'When the Lights Go Out', 'Got the Feelin\'', 'Everybody Get Up', 'It\'s the Things You Do', 'Until the Time Is Through', 'If Ya Gettin\' Down', 'Keep On Movin\'', 'Don\'t Wanna Let You Go', 'We Will Rock You', 'Let\'s Dance', 'Closer to Me', 'Rock the Party', 'I Wish It Could Be Christmas Everyday'];
   }
 
+  five.elements = function() {
+    return ['Earth', 'Water', 'Fire', 'Wind', 'Void'];
+  }
+
   five.furious = function() {
     return ['Tigress','Viper','Crane','Monkey','Mantis'];
   };
--- test.js
+++ test.js
@@ -116,6 +116,8 @@ assert.equal(JSON.stringify(['5ive', 'Invincible', 'Kingsize']), JSON.stringify(
 
 assert.equal(JSON.stringify(['Slam Dunk (Da Funk)', 'When the Lights Go Out', 'Got the Feelin\'', 'Everybody Get Up', 'It\'s the Things You Do', 'Until the Time Is Through', 'If Ya Gettin\' Down', 'Keep On Movin\'', 'Don\'t Wanna Let You Go', 'We Will Rock You', 'Let\'s Dance', 'Closer to Me', 'Rock the Party', 'I Wish It Could Be Christmas Everyday']), JSON.stringify(five.singles()), 'A five singles should be the singles released by the 90\'s boy band 5ive');
 
+assert.equal(JSON.stringify(['Earth', 'Water', 'Fire', 'Wind', 'Void']), JSON.stringify(five.elements()), 'The five elements philosophy in Japanese Buddhism, derived from Buddhist beliefs');
+
 assert.equal(JSON.stringify(['Tigress','Viper','Crane','Monkey','Mantis']), JSON.stringify(five.furious()), 'A Furious five should be the five fictional members of the ninja group Furious Five from the movie Kung-Fu Panda');
 
 assert.equal(five.luniz(), 'I Got 5 on It', 'A Luniz five should be the song title of their most famous hit');
