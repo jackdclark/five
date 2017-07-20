831edc07eb2d2b7507e654166446795e90f9e645
guifl <fguillemot.external@lafourchette.com>
2017-07-20 15:09:36 +0200

    correction of my previous tests about € and $

--- test.js
+++ test.js
@@ -131,9 +131,9 @@ assert.equal(5, five.reduce([1, 2, 3]));
 
 assert.equal('£5', five.r(), 'A fiver should be 5 GBP');
 
-asser.equal('5€', five.euro());
+assert.equal('5€', five.euro());
 
-asser.equal('5$', five.dollar());
+assert.equal('5$', five.dollar());
 
 assert.equal('5678901234', five.rot('0123456789'), 'Numbers should be rotated');
 assert.equal('fghijklmnopqrstuvwxyzabcde', five.rot('abcdefghijklmnopqrstuvwxyz'), 'Small letters should be rotated');
