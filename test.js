bdd1161fff76baca3ebc53ca33141a527d52bb13
guifl <fguillemot.external@lafourchette.com>
2017-07-20 15:05:33 +0200

    test for five.euro() //5€

--- test.js
+++ test.js
@@ -131,6 +131,8 @@ assert.equal(5, five.reduce([1, 2, 3]));
 
 assert.equal('£5', five.r(), 'A fiver should be 5 GBP');
 
+asser.equal('5€', five.euro());
+
 assert.equal('5678901234', five.rot('0123456789'), 'Numbers should be rotated');
 assert.equal('fghijklmnopqrstuvwxyzabcde', five.rot('abcdefghijklmnopqrstuvwxyz'), 'Small letters should be rotated');
 assert.equal('FGHIJKLMNOPQRSTUVWXYZABCDE', five.rot('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'Capital letters too');
